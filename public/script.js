4477c6d831eec2b8bb68e96965e174345d72ae34
projectcamar <53714437+projectcamar@users.noreply.github.com>
2024-09-26 02:11:57 +0700

    Update script.js

--- public/script.js
+++ public/script.js
@@ -335,3 +335,36 @@ document.addEventListener('keydown', event => {
         event.preventDefault();
     }
 });
+
+var modal = document.getElementById("image-modal");
+
+// Get the image and insert it inside the modal
+var modalImg = document.getElementById("full-size-image");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// Function to open the modal
+function openModal(img) {
+    modal.style.display = "block";
+    modalImg.src = img.src;
+}
+
+// Add click event to all post-logo and mentor-img elements
+document.querySelectorAll('.post-logo, .mentor-img').forEach(img => {
+    img.onclick = function() {
+        openModal(this);
+    }
+});
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+    modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
